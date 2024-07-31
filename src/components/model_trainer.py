f51c998ca5fa19e2f07d00be4da5c0096f01d3b4
OmB <ombansod112@gmail.com>
2024-07-31 19:08:15 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,24 +17,24 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object,evaluate_models
+from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config=ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Split training and test input data")
-            X_train, y_train,X_test,y_test = (
-                train_array[:,:-1],
-                train_array[:,-1],
-                test_array[:,:-1],
-                test_array[:,-1]
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
             )
             models = {
                 "RandomForest": RandomForestRegressor(),
@@ -45,29 +45,62 @@ class ModelTrainer:
                 "DecisionTree": DecisionTreeRegressor(),
                 "XGBoost": XGBRegressor(),
                 "CatBoost": CatBoostRegressor(),
-            }    
+            }
+            params = {
+                "DecisionTree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "RandomForest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "GradientBoosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                },
+                "LinearRegression": {},
+                "KNeighbors": {
+                    'n_neighbors': [5, 7, 9, 11],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                "XGBoost": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'learning_rate': [.1, .01, .05, .001],
+                },
+                "CatBoost": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'learning_rate': [.1, .01, .05, .001],
+                }
+            }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param=params)
 
             best_model_score = max(sorted(model_report.values()))
-
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
-
             best_model = models[best_model_name]
 
-            if best_model_score<0.6:
+            if best_model_score < 0.6:
                 raise CustomException("No best model found")
-            logging.info(f"Best found model on both traing and testing dataset")
+
+            logging.info(f"Best model found: {best_model_name} with score: {best_model_score}")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
 
-            predictted=best_model.predict(X_test)
+            predicted = best_model.predict(X_test)
+            score = r2_score(y_test, predicted)
+            logging.info(f"R-squared score of the best model: {score}")
 
-            r2_score = r2_score(y_test,predictted)
-            return r2_score
+            return score
 
-        except:
-            pass
+        except Exception as e:
+            logging.error(f"Error occurred: {e}")
+            raise CustomException(f"Error occurred: {e}")
